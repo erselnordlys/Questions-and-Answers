eff9121d52f7d5a04b15d2a974f26923d13b3026
Alla P <evetrueivory@gmail.com>
2017-05-22 19:10:03 +0300

    отредактировала разметку для pop-up страницы. и теперь она появляется по клику на ответы первого вопроса и исчезает по клику на крестик.

---
+++ scripts.js
@@ -0,0 +1,35 @@
+
+
+var x = document.querySelectorAll('.answer-block');
+
+
+window.onload = main;
+
+
+
+// показать/скрыть поп ап
+function displayPopUp() {
+    document.getElementById('pop-up-page').classList.toggle('visible');
+}
+
+
+function addListenersToAnswerBlocks(total) {
+    for (var i = 0; i < total; i++) {
+        document.querySelectorAll('.answer-block')[i].onclick = function () { displayPopUp(); };
+    }
+
+    document.getElementById('close-icon').onclick = function () { displayPopUp(); }
+}
+
+
+
+
+function main() {
+    var x = document.querySelectorAll('.lc__quest-block-1 .answer-block'); // работает только для 1 вопроса
+    var length = x.length;
+    console.log(length);
+
+    addListenersToAnswerBlocks(length);
+
+}
+
