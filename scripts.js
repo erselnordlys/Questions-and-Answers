bcf612c1df0505ae97a26ca44eb4456278cee03c
Alla P <evetrueivory@gmail.com>
2017-05-24 22:38:45 +0300

    первый вариант анимации, работает только 1 раз

--- scripts.js
+++ scripts.js
@@ -9,17 +9,37 @@ window.onload = main;
 
 // показать/скрыть поп ап
 function displayPopUp() {
-    document.getElementById('pop-up-page').classList.toggle('visible');
-    document.getElementById('main-page-content').classList.toggle('visible');
+    document.getElementById('pop-up-wrapper').classList.toggle('visible');
+
 }
 
+function displayMainContent() {
+    document.getElementById('main-page-content').classList.toggle('visible');
+
+}
 
 function addListenersToAnswerBlocks(total) {
     for (var i = 0; i < total; i++) {
-        document.querySelectorAll('.answer-block')[i].onclick = function () { displayPopUp(); };
+        document.querySelectorAll('.answer-block')[i].onclick = function () {
+            displayPopUp();
+            startTimer(displayMainContent);
+            document.getElementById('main-page-content').style.animation = 'fadeOut 600ms'; // исчезает контент
+
+        };
     }
 
-    document.getElementById('close-icon').onclick = function () { displayPopUp(); }
+
+    document.getElementById('close-icon').onclick = function () {
+        startTimer(displayPopUp);
+        // startTimer(displayMainContent);
+        displayMainContent();
+        document.getElementById('pop-up-page').style.animation = 'pop-up-disappear 600ms';
+        document.getElementById('pop-up-wrapper').style.animation = 'fadeOut 600ms';
+
+        document.getElementById('main-page-content').style.animation = 'fadeIn 600ms';
+
+
+    }
 }
 
 
@@ -33,4 +53,9 @@ function main() {
     addListenersToAnswerBlocks(length);
 
 }
+
+function startTimer(myFunc) {
+    setTimeout( function () { myFunc(); console.log('0.6s passed')}, 600);
+}
+
 
